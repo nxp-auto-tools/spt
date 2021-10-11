3de14286ffd9f04764e832e70f48b1cfb2ba82da
Sergiu Crisan <[sergiu-dan.crisan@codeaurora.org]sergiu-dan.crisan@codeaurora.org>
2021-10-11 17:44:57 +0300

    CAF commit for SPT3.1_2021.10.8

---
+++ build/linux/lin_spt.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ "${ADL_ROOT}" = "" ]           && ADL_ROOT="/opt/adl-3.23.11"
+[ "${LIN_CXX}" = "" ]            && LIN_CXX="/usr/bin/g++"
+[ "${LIN_CC}" = "" ]             && LIN_CC="/usr/bin/gcc"
+[ "${CLEANUP_DIRS}" = "" ]       && CLEANUP_DIRS=0
+
+export CC=${LIN_CC}
+export CXX=${LIN_CXX}
+
+export ROOT=${ADL_ROOT}
+
+cd tools_spt/dev
+make all
+
+cd ..
+mkdir -p -m777 SPT3/bin
+mkdir -p -m777 SPT3/inc
+mkdir -p -m777 SPT3/doc
+#copy binaries
+cp dev/as-spt3 SPT3/bin/as-spt
+cp dev/objdump-spt3 SPT3/bin/objdump-spt
+#copy includes directory
+cp -r inc/*3.inc SPT3/inc
+#copy documentation
+cp doc/as-spt3-manual.pdf SPT3/doc
+cp doc/'SPT3 mnemonics QuickRef' SPT3/doc
+cp doc/gas-manual.pdf SPT3/doc
+cp doc/ReadMe.spt3.pdf SPT3/ReadMe.pdf
+#prepare artifact
+BUILD_DATE=`date +%y%m%d`
+zip -q -r SPT_${BUILD_DATE}_linux.zip SPT3
+zip -q -r adl_src.zip dev/*.c* dev/*.h
