ade6e28b776d51a46bc7d57dff26eba572bd7c25
Sergiu Crisan <[sergiu-dan.crisan@codeaurora.org]sergiu-dan.crisan@codeaurora.org>
2021-10-27 12:34:51 +0300

    CAF commit for SPT3.5_2021.10.26

---
+++ dev/spt_ds.cpp
@@ -0,0 +1,21 @@
+//
+// Copyright 2020-2021 NXP
+// SPDX-License-Identifier: GPL-3.0+
+//
+
+//
+// Extra disassembler support for SPT3.5.
+//
+
+extern "C" {
+# include "config.h"
+}
+
+#include "dis-asm.h"
+
+
+void disassemble_init_adl (struct disassemble_info * info)
+{
+   info->skip_zeroes = 16;
+}
+
